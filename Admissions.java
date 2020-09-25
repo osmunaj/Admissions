b5831c56cddd5e5aaa1a5485df660c5b3e714dc7
osmunaj <56142640+osmunaj@users.noreply.github.com>
2020-09-25 10:35:36 -0600

    Add files via upload

---
+++ Admissions.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+//A. Elete level in at least one
+//B. Solid level in two
+//C. Last name is Padjent
+//D. Nothing below acceptable
+
+
+public class Admissions {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("What is your Name?");
+        String name = scanner.next();
+
+        System.out.println("What is your SAT Score?");
+        int satScore = scanner.next();
+
+        System.out.println("What is your class ranking?");
+         
+        
+    }
+}
